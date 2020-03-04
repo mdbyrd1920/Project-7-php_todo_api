cc897aef8d7f27c3bec5b02144f1e2070c5a6ea6
mdbyrd1920 <53909144+mdbyrd1920@users.noreply.github.com>
2020-03-04 12:33:52 -0500

    Add files via upload

--- src/dependencies.php
+++ src/dependencies.php
@@ -23,7 +23,7 @@ $container['logger'] = function ($c) {
 //API
 $container['api'] = function($c) {
 $api = $c->get('settings') ['api'];
-$api['api_url'] = $api['base_url']. '/api/'.$api['verison'];
+$api['api_url'] = $api['base_url']. '/api/'.$api['version'];
 return $api;
 };
 
--- src/routes.php
+++ src/routes.php
@@ -7,22 +7,23 @@ use Slim\Http\Response;
 
 $app->get('/', function ($request, $response, $args) {
   $endpoints = [
-    'all tasks' => $this->api['api_url'].'/tasks',
-    'single task' => $this->api['api_url'].'/tasks/{task_id}',
-    'review by task' => $this->api['api_url'].'/tasks/{task_id}/reviews',
-    'single review' => $this->api['api_url'].'/tasks/{task_id}/reviews/{review_id}',
+    'all tasks' => $this->api['api_url'].'/todos',
+    'single task' => $this->api['api_url'].'/todos/{task_id}',
+  /*  'review by task' => $this->api['api_url'].'/tasks/{task_id}/reviews',
+    'single review' => $this->api['api_url'].'/tasks/{task_id}/reviews/{review_id}',*/
     'help' => $this->api['base_url']. '/',
 
   ];
   $result = [
     'endpoints' => $endpoints,
-    'verison' => $this->api['version'],
+    'version' => $this->api['version'],
     'timestamp' => time(),
   ];
   return $response->withJson($result, 200, JSON_PRETTY_PRINT);
 });
 
-$app->group('api/v1/tasks', function() use($app) {
+$app->group('api/v1/todos', function() use($app) {
+
   $app->get('', function ($request, $response, $args) {
   $result = $this->task->getTasks();
   return $response->withJson($result, 200, JSON_PRETTY_PRINT);
@@ -33,19 +34,46 @@ $app->group('api/v1/tasks', function() use($app) {
   return $response->withJson($result, 200, JSON_PRETTY_PRINT);
   });
 
-  $app->group('/{task_id}/reviews', function() use($app) {
+  $app->post('', function ($request, $response, $args) {
+      $data = $request->getParsedBody();
+      $data['task_id'] = $args['task_id'];
+      $result = $this->task->createTask($data);
+      return $response->withJson($result, 201, JSON_PRETTY_PRINT);
+  });
+  $app->put('/{task_id}', function ($request, $response, $args) {
+      $data = $request->getParsedBody();
+      $data['task_id'] = $args['task_id'];
+      $result = $this->task->updateTask($data);
+      return $response->withJson($result, 201, JSON_PRETTY_PRINT);
+  });
+  $app->delete('/{task_id}', function ($request, $response, $args) {
+      $result = $this->task->deleteTask($args['task_id']);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+  });
+
+
+
+
+
+
+
+
+
+
+/*
+$app->group('/{task_id}/subtask', function() use($app) {
   $app->get('', function ($request, $response, $args) {
   $result = $this->review->getReviewsByTaskId($args['task_id']);
   return $response->withJson($result, 200, JSON_PRETTY_PRINT);
 
   });
 //refer to route grup course to finish
-  $app->get('/{task_id}/reviews', function ($request, $response, $args) {
+  $app->get('/{task_id}/subtask', function ($request, $response, $args) {
   $result = $this->review->getReviewsByTaskId($args['task_id']);
   return $response->withJson($result, 200, JSON_PRETTY_PRINT);
 
   });
-    });
+});*/
 
 
 
